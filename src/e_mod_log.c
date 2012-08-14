fc90367f31b50bd6b8bb3f1cd6b160e92e116aae
Deon 'PrinceAMD' Thomas <PrinceAMD.Elive@Gmail.com>
2012-08-14 11:00:24 -0400

    Added eina log domain callback function

---
+++ src/e_mod_log.c
@@ -0,0 +1,44 @@
+#include <e.h>
+#include "e_mod_main.h"
+
+void
+e_mod_log_cb(const Eina_Log_Domain *d, Eina_Log_Level level, const char *file,
+              const char *fnc, int line, const char *fmt, void *data, va_list args)
+{
+   if ((d->name) && (d->namelen == sizeof("E_PRODUCTIVITY") - 1) &&
+       (memcmp(d->name, "E_PRODUCTIVITY", sizeof("E_PRODUCTIVITY") - 1) == 0))
+     {
+        const char *prefix;
+        Eina_Bool use_color = !eina_log_color_disable_get();
+
+        if (use_color)
+          fputs(eina_log_level_color_get(level), stderr);
+
+        switch (level)
+          {
+           case EINA_LOG_LEVEL_CRITICAL:
+              prefix = "Critical. ";
+              break;
+           case EINA_LOG_LEVEL_ERR:
+              prefix = "Error. ";
+              break;
+           case EINA_LOG_LEVEL_WARN:
+              prefix = "Warning. ";
+              break;
+           default:
+              prefix = "";
+          }
+        fprintf(stderr, "%s: %s", "E_PRODUCTIVITY", prefix);
+
+        if (use_color)
+          fputs(EINA_COLOR_RESET, stderr);
+
+
+        vfprintf(stderr, fmt, args);
+        putc('\n', stderr);
+     }
+   else
+     eina_log_print_cb_stderr(d, level, file, fnc, line, fmt, NULL, args);
+}
+
+
